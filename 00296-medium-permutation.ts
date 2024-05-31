2234f6050b5af75eb16db0fb15446478b2a52332
sunven <604164227@qq.com>
2024-05-31 23:42:59 +0800

    Permutation

--- 00296-medium-permutation.ts
+++ 00296-medium-permutation.ts
@@ -20,4 +20,8 @@ type cases = [
 ]
 
 // ============= Your Code Here =============
-type Permutation<T> = T extends T ? [T, ...Permutation<Exclude<T, T>>] : []
+// 官方文档中，介绍了一种操作，叫 Distributive conditional types
+// 简单来说，传入给T extends U中的T如果是一个联合类型A | B | C，则这个表达式会被展开成
+// (A extends U ? X : Y) | (B extends U ? X : Y) | (C extends U ? X : Y)
+// [U] extends [never] 而不是 U extends never 因为  U是联合类型 条件类型会走分配得到的是一个联合类型  不符合期望
+type Permutation<T, U = T> = [U] extends [never] ? [] : T extends U ? [T, ...Permutation<Exclude<U, T>>] : []
