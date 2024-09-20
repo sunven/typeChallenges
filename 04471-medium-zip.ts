ec1e6fa978953ae49294f299a4016118310408f5
sunven <604164227@qq.com>
2024-09-20 21:05:36 +0800

    feat: Zip

---
+++ 04471-medium-zip.ts
@@ -0,0 +1,13 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Zip<[], []>, []>>,
+  Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
+  Expect<Equal<Zip<[1, 2, 3], ['1', '2']>, [[1, '1'], [2, '2']]>>,
+  Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
+  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>
+]
+
+// ============= Your Code Here =============
+type Zip<T, U> = T extends [infer A, ...infer R] ? (U extends [infer B, ...infer S] ? [[A, B], ...Zip<R, S>] : []) : []
